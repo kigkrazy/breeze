6f8edff7305947b792fcadc7166b3f4db6335b85
kigkrazy <kigkrazy@163.com>
2019-01-23 13:58:05 +0800

    ADD login mapper

--- src/main/java/com/reizx/breeze/modules/sys/controller/SysLogin.java
+++ src/main/java/com/reizx/breeze/modules/sys/controller/SysLogin.java
@@ -1,9 +1,13 @@
 package com.reizx.breeze.modules.sys.controller;
 
 import com.reizx.breeze.utils.R;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/sys")
 public class SysLogin {
-
+    @GetMapping(value = "/login")
+    public R login() {
+        return R.ok();
+    }
 }
