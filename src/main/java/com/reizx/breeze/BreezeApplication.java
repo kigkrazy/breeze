afa82750873a299adcfbe1a7a8d66468b6859ec9
kigkrazy <kigkrazy@163.com>
2019-01-24 15:24:52 +0800

    fix DataSource cycle bug

--- src/main/java/com/reizx/breeze/BreezeApplication.java
+++ src/main/java/com/reizx/breeze/BreezeApplication.java
@@ -1,9 +1,13 @@
 package com.reizx.breeze;
 
+import com.reizx.breeze.datasources.DynamicDataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Import({DynamicDataSourceConfig.class})
 public class BreezeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BreezeApplication.class, args);
