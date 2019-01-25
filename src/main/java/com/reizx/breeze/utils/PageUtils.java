aa069082b245388f953709e4b4966b30d9b27917
kigkrazy <kigkrazy@163.com>
2019-01-25 14:20:19 +0800

    add PageUtils

---
+++ src/main/java/com/reizx/breeze/utils/PageUtils.java
@@ -0,0 +1,54 @@
+package com.reizx.breeze.utils;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 分页工具类
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2016年11月4日 下午12:59:00
+ */
+@Data
+public class PageUtils implements Serializable {
+    private long totalCount;//总记录数
+    private long pageSize;//每页记录数
+    private long totalPage;//总页数
+    private long currPage;//当前页数
+    private List<?> list;//列表数据
+
+    /**
+     * 分页
+     *
+     * @param list       列表数据
+     * @param totalCount 总记录数
+     * @param pageSize   每页记录数
+     * @param currPage   当前页数
+     */
+    public PageUtils(List<?> list, int totalCount, int pageSize, int currPage) {
+        this.list = list;
+        this.totalCount = totalCount;
+        this.pageSize = pageSize;
+        this.currPage = currPage;
+        this.totalPage = (int) Math.ceil((double) totalCount / pageSize);
+    }
+
+    /**
+     * 分页
+     *
+     * @param page 分页
+     */
+    public PageUtils(Page<?> page) {
+        this.list = page.getRecords();
+        this.totalCount = page.getTotal();
+        this.pageSize = page.getSize();
+        this.currPage = page.getCurrent();
+        this.totalPage = page.getPages();
+    }
+
+
+}
